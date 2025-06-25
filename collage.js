aa9cbef34560454367c74b93651a7ff224ff6814
Yuvi4356 <yuva43562@gmail.com>
2025-06-25 10:04:50 +0530

    Create collage.js

---
+++ collage.js
@@ -0,0 +1,18 @@
+// data types
+let number = 44;
+let StringVar = "this an section of java class";
+let BooleanVar = true;
+let undefinedVar;
+let nulVar = null;
+let SymbolVar = Symbol("unique");
+let bigIntVar =BigInt(1214541444545477851555);
+let objectVar = {key:"value"};
+ //output 
+ console.log(number,StringVar,BooleanVar,SymbolVar,bigIntVar,objectVar);
+  
+ let obj2 ={
+    name:"naveen",
+    age:20,
+    isstudent:false,
+ }
+   console.log(obj2);
