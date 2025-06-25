7635e0d0be8d180b6e67835343e49b503ac6126a
Yuvi4356 <yuva43562@gmail.com>
2025-06-25 10:24:56 +0530

    Create controal.js

---
+++ controal.js
@@ -0,0 +1,115 @@
+// //  example of different data types in javascript
+
+// // Primitive data types
+// //let number = 42; //number
+// //let string = "Hello world"; //string
+// //et boolean = true; //Boolean
+// //let undefinedVar; //undefined //(not assigned a value)
+// //let nullVar = null; //Null
+// //let symbolVar = symbool("unique"); //symbol //(unique identifier) // (often used as object property keys)
+// //let bigIntVar = BigInt(123456789012345678901234567890); //used for large integers
+
+// //non-primitive data types
+// //let objectVar = { key: "value"}; //object// (collection key-value pairs)
+// //let arrayVar  = {1,2,3,4,5}; //Array //(ordered collection of values)
+// //let FunctionVar = function() {return "I am a function";}
+
+// //let object2={
+//  //   name:"John Doe",
+//  //   age : 30;
+// //    isEmployed:true,
+// //    hobbies:["reading","gaming","coding"]
+// //}
+// //
+// //output the data types
+// console.log("Data Types in Javascript:");
+// console.log("Number:", number);
+// console.log("String:", string)
+// console.log("Boolean:", boolean);
+// console.log("Undefined:", undefined);
+// console.log("Null:", nullVar);
+// console.log("Symbol:")
+// console.log()
+
+
+
+// //1.arihtmetic operators
+// let a = 10;
+// let b = 20;
+// console.log("Arithmetic operators :");
+// console.log("a =",a,"b =",b);
+// let s = a + b;
+// let d = a - b ;
+// let pr = a * b ;
+// let div  = a/b;
+// let rem = b%a;
+// let exp = a ** b;
+// console.log(" sum is :",s);
+// console.log("difference is :",d);
+// console.log("product is :",pr);
+// console.log("divsion :",div);
+// console.log("a module b :",rem);
+// console.log("a exponent b is :",exp);
+
+
+// //2.Assignment operators
+// let x = 10;
+// console.log("initial x is :",x);
+// x +=5;
+// x -=3;
+// x *=2;
+// x /=4;
+// x %=3;
+// console.log("Assignment oprators")
+// console.log("x after assignmentsm: ",x);
+// console.log("\n")
+
+// //3.Comparison operators
+// let isEqual = (a == b);
+// let isStrictEqual = ( a === b);
+// let isNotEqual = (a != b);
+// let isStrictNotEqual = (a !== b);
+// let isGreaterThan = (a > b);
+// let isLessThan = (a < b);
+// let isGreaterThanOrEqual = (a >= b);
+// let isLessThanOrEqual = (a <= b);
+// console.log("comparison operators");
+// console.log("isEqual",isEqual);
+// console.log("isStrictEqual",isStrictEqual);
+// console.log("isNotEqual",isNotEqual);
+// console.log("isStrictNotEqual",isStrictNotEqual);
+// console.log("isGreaterThan",isGreaterThan);
+// console.log("isLessThan",isLessThan);
+// console.log("isGreaterThanOrEqual",isGreaterThanOrEqual);
+// console.log("isLessThanOrEqual",isLessThanOrEqual);
+
+// //4.logical operators
+// let andOperator = (a >0 && b >0);
+// let orOperator = (a > 0 || b >0);
+// let notOperator = !a;
+// console.log("Logical Operators");
+// console.log("andOperator",andOperator);
+// console.log("orOperator",orOperator);
+// console.log("notOperator",notOperator);
+
+// //5.Bitwise Operators
+// let bitwiseAnd = a & b;
+// let bitwiseOr = a | b ;
+// let bitwiseXor = a ^ b ;
+// let bitwiseNot = ~a ;
+// let leftshift =a << 1;
+// let rightshift =a >> 1;
+// console.log("Bitwise Operators");
+// console.log("bitwiseAnd",bitwiseAnd);
+// console.log("bitwiseOr",bitwiseOr);
+// console.log("bitwiseXor",bitwiseXor);
+// console.log("bitwiseNot",bitwiseNot);
+// console.log("leftshift",leftshift);
+// console.log("rightshift",rightshift);
+
+//6.terminary operator
+let age=18;
+let canVote =(age >= 18) ? "allowed to vote":"not allowed to vote";
+console.log("terminary");
+console.log(eligible);
+
